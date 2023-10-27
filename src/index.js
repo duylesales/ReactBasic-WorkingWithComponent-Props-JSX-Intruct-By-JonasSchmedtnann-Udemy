f9232317275be6d5fe669a98d22987cd65f8a015
Duy Node <mac@Duy-Node.local>
2023-10-27 11:19:11 +0700

    How to use React Fragments

--- src/index.js
+++ src/index.js
@@ -75,15 +75,18 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>Delicious Italian Food</p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
-      ;
     </main>
   );
 }
