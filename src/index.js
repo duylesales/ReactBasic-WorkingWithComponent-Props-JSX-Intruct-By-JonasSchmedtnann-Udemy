11ecb029efd8248a3b25eae4a4dc897dcaab2d8e
Duy Node <mac@Duy-Node.local>
2023-10-27 09:46:41 +0700

    Multi Returns

--- src/index.js
+++ src/index.js
@@ -89,6 +89,7 @@ function Menu() {
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -103,19 +104,24 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 8;
+  const openHour = 10;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
   // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
   // else alert("Sorry we're closed ");
+  // if (!isOpen)
+  //   return (
+  //     <p>
+  //       We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+  //     </p>
+  //   );
+
   return (
     <footer className="footer">
       {isOpen ? (
         <div className="order">
-          <p>
-            We're open until {closeHour}:00. Come visit us or order online.{" "}
-          </p>
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
       ) : (
